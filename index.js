cef6b961c840b8c694eef28435a6d059e0c02169
Natasha Beddow <Beddow90@gmail.com>
2020-11-20 14:40:03 +0000

    Updated 201120

--- index.js
+++ index.js
@@ -1,48 +1,62 @@
 
+//To-Do
+//Figure out how to remove document statement from repeating function
+
 // Stopwatch object
 var sw = {
 cs: 0,
 s: 0,
 m: 0,
-h: 0
+h: 0,
 };
 
-document.getElementById("stopwatch").innerHTML = display();
+var face = document.getElementById("stopwatch");
 
 // Increments units of time
 function timer() {
     sw.cs++;
-        if(sw.cs == 99) {
+        if (sw.cs == 99) {
             sw.s++;
             sw.cs = 0;
         }
-        if(sw.s == 59) {
+        if (sw.s == 59) {
             sw.m++;
             sw.s = 0;
         }
-        if(sw.m == 59) {
+        if (sw.m == 59) {
             sw.h++;
             sw.m = 0;
-        }
+        };
+        display();
 };
 
-var display = function() {
-    if(sw.cs < 10) {
+//Adds and removes 0s where necessary
+function display() {
+    if (sw.cs < 10) {
         sw.cs = "0" + String(sw.cs);
-}   if (sw.cs >= 10) {
+}   else if (sw.cs >= 10) {
         sw.cs = String(sw.cs);
 }   if (sw.s < 10) {
         sw.s = "0" + String(sw.s);
-}   if (sw.s >= 10) {
+}   else if (sw.s >= 10) {
         sw.s = String(sw.s);
-}   return sw.s + " : " + sw.cs;  
+}   if (sw.m < 10) {
+        sw.m = "0" + String(sw.m);
+}   else if (sw.m >= 10) {
+        sw.m = String(sw.m);
+} face.innerHTML = sw.m + " : " + sw.s + " : " + sw.cs;
+//Prevents multiple 0s being added to s
+    if (sw.s.substring(0, 1) == "0") {
+        sw.s = sw.s.substring(1);
+    }
+    if (sw.m.substring(0, 1) == "0") {
+        sw.m = sw.m.substring(1);
+    }
 };
 
 // Starts the timer upon clicking Start button in HTML
 var start = function() {
     setInterval(timer, 10);
-    document.getElementById("stopwatch").innerHTML = display();
-
 };
 
 // Stops the timer upon clicking Stop button in HTML
